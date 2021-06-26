f663254299415016c27648387fa56dcdb751c0a7
jkchao <jkchaom@gmail.com>
2021-06-26 09:50:32 +0800

    feat: merge sorted array

--- js/merge-sorted-array.js
+++ js/merge-sorted-array.js
@@ -9,5 +9,33 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1, m, nums2, n) {
-    
+    let i = m - 1;
+    let j = n - 1;
+    let t = m + n - 1;
+
+    while(t >= 0) {
+        if (i < 0) {
+            nums1[t] = nums2[j];
+            j --;
+        }
+        else if (j < 0) {
+            nums1[t] = nums1[i];
+            i --;
+        }
+        else if (nums1[i] > nums2[j]) {
+            nums1[t] = nums1[i];
+            i--;
+        } else {
+            nums1[t] = nums2[j];
+            j--;
+        }
+
+        t --;
+    }
+
+
+    console.log(nums1);
 }
+
+
+merge([1,2,3,0,0,0], 3, [2,5,6], 3);
