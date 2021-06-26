102a9ddbb9e494648ca3a3a4f0828fb65af66668
jkchao <jkchaom@gmail.com>
2021-06-26 16:21:16 +0800

    fix

--- js/merge-sorted-array.js
+++ js/merge-sorted-array.js
@@ -8,13 +8,18 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1, m, nums2, n) {
-
     let i = m - 1;
     let j = n - 1;
     let t = m + n - 1;
 
-    while (j >= 0) {
-        if (i >= 0 && nums1[i] >= nums2[j]) {
+    while (t >= 0) {
+        if (i < 0) {
+            nums1[t] = nums2[j];
+            j--;
+        } else if (j < 0) {
+            nums1[t] = nums1[i];
+            i--;
+        } else if (nums1[i] > nums2[j]) {
             nums1[t] = nums1[i];
             i--;
         } else {
@@ -26,3 +31,4 @@ function merge(nums1, m, nums2, n) {
     }
 }
 
+merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
--- rs/src/merge_sorted_array.rs
+++ rs/src/merge_sorted_array.rs
@@ -1,15 +1,25 @@
 #[allow(dead_code)]
 pub fn merge_sorted_arrays(nums1: &mut Vec<i32>, m: i32, nums2: Vec<i32>, n: i32) {
-    let (mut m, mut n) = (m as usize, n as usize);
 
-    while n > 0 {
-        if m > 0 && nums1[m - 1] > nums2[n - 1] {
-            nums1[m + n - 1] = nums1[m - 1];
-            m -= 1;
+    let mut i = m - 1;
+    let mut j = n - 1;
+    let mut t = m + n - 1;
+    while t >= 0 {
+        if i < 0 {
+            nums1[t as usize] = nums2[j as usize];
+            j -= 1;
+        } else if j < 0 {
+            nums1[t as usize] = nums1[i as usize];
+            i -= 1;
+        } else if nums1[i as usize] > nums2[j as usize] {
+            nums1[t as usize] = nums1[i as usize];
+            i -= 1;
         } else {
-            nums1[m + n - 1] = nums2[n - 1];
-            n -= 1;
+            nums1[t as usize] = nums2[j as usize];
+            j -= 1;
         }
+
+        t -= 1;
     }
 }
 
