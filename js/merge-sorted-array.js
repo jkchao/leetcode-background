2a22c06a5710ddda1ff7ebbb8625f4d3f659f182
jkchao <jkchaom@gmail.com>
2021-06-26 17:12:28 +0800

    refactor

--- js/merge-sorted-array.js
+++ js/merge-sorted-array.js
@@ -13,18 +13,12 @@ function merge(nums1, m, nums2, n) {
     let t = m + n - 1;
 
     while (t >= 0) {
-        if (i < 0) {
+        if (i < 0 || nums1[i] < nums2[j]) {
             nums1[t] = nums2[j];
             j--;
-        } else if (j < 0) {
-            nums1[t] = nums1[i];
-            i--;
-        } else if (nums1[i] > nums2[j]) {
-            nums1[t] = nums1[i];
-            i--;
         } else {
-            nums1[t] = nums2[j];
-            j--;
+            nums1[t] = nums1[i];
+            i--;
         }
 
         t--;
