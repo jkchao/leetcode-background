af7ca0b75fc30d5e9b749384101509d5d659f5c1
jkchao <jkchaom@gmail.com>
2021-08-30 23:06:41 +0800

    refactor: longest-subtring-without-repeating-characters

--- js/src/longest-substring-without-repeating-characters.js
+++ js/src/longest-substring-without-repeating-characters.js
@@ -4,46 +4,6 @@
  * @param {string} s
  * @return {number}
  */
-// function lengthOfLongestSubstring(s) {
-//     if (s.length === 0) {
-//         return 0;
-//     };
-
-//     if (s.length === 1) {
-//         return 1;
-//     }
-
-//     let set = new Set();
-//     let j = 1;
-//     let result = 0;
-
-//     set.add(s.charAt(0));
-
-//     for (let i = 0; i < s.length - 1; i++) {
-//         if (i !== 0) {
-//             set.delete(s.charAt(i - 1));
-//         };
-
-//         while(j < s.length) {
-//             if (set.has(s.charAt(j))) {
-//                 break;
-//             } else {
-//                 set.add(s.charAt(j));
-//             }
-//             j++;
-//         }
-
-//         result = Math.max(result, set.size);
-//     }
-//     return result;
-// }
-
-
-/**
- * @param {string} s
- * @return {number}
- */
-
 function lengthOfLongestSubstring(s) {
     if (s.length === 0) {
         return 0;
@@ -54,21 +14,26 @@ function lengthOfLongestSubstring(s) {
     }
 
     let set = new Set();
-    let i = 0;
-    let j = 0;
+    let j = 1;
     let result = 0;
 
+    set.add(s.charAt(0));
 
-    while(j < s.length) {
-        set.add(s.charAt(j));
-        j++;
+    for (let i = 0; i < s.length - 1; i++) {
+        if (i !== 0) {
+            set.delete(s.charAt(i - 1));
+        };
 
-        while(set.has(s.charAt(j))) {
-            set.delete(s.charAt(i));
-            i++;
+        while(j < s.length) {
+            if (set.has(s.charAt(j))) {
+                break;
+            } else {
+                set.add(s.charAt(j));
+            }
+            j++;
         }
+
         result = Math.max(result, set.size);
     }
+    return result;
 }
-
-lengthOfLongestSubstring("pwwkew");
--- js/src/minimum-size-subarray-sum.js
+++ js/src/minimum-size-subarray-sum.js
@@ -28,5 +28,3 @@
 
 console.log(minSubArrayLen(7, [2,3,1,2,4,3]));
 console.log(minSubArrayLen(4, [1,4,4]));
-console.log(minSubArrayLen(11, [1,1,1,1,1,1,1,1]));
-console.log(minSubArrayLen(11, [1,2,3,4,5]));
